0bc761c54bf23ff56a21a329568bf4dad228657b
alfonsoIodiceDE <98343566+alfonsoIodiceDE@users.noreply.github.com>
2026-01-18 10:22:26 +0100

    Update spectral_cluster_scipt_cristina.R

--- spectral_cluster_experiments/spectral_cluster_scipt_cristina.R
+++ spectral_cluster_experiments/spectral_cluster_scipt_cristina.R
@@ -1,32 +1,18 @@
-# set.seed(123)
-# library(mvtnorm)
-# library(dplyr)
-# library(aricode)
-# library(cluster)
-# library(mvtnorm)
-# library(dplyr)
-# library(manydist)
-# library(tidyverse)
-# library(FPDclustering)
-# source("R/delta_knn_ba.R")
-# pengs = palmerpenguins::penguins |> na.omit()
-#
-# data(Star)
-#
-# # load(file="Xkp.RData")
-# # load(file="XkpEasy.rdata")
-# # load(file="XkpMedium.rdata")
-# #
-# dfc = XkpMedium |> as_tibble()
-# dfc = pengs |> as_tibble() |> select(-species,-year)
-#
-# star = Star |> select(-Type) |> mutate(across(where(is.character), as.factor))
+set.seed(123)
+library(mvtnorm)
+library(dplyr)
+library(aricode)
+library(cluster)
+library(mvtnorm)
+library(dplyr)
+library(manydist)
+library(tidyverse)
+library(FPDclustering)
+library(tidyverse)
+library(tidymodels)
+library(palmerpenguins)
 
 
-# options(repos = c(CRAN = "https://cloud.r-project.org"))
-#
-# install.packages(c("parallelDist","klaR","arules","FD","StatMatch","clustMixType"))
-#
 remotes::install_github("alfonsoIodiceDE/manydist_package", subdir = "manydist")
 
 if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
@@ -37,12 +23,8 @@ if (!requireNamespace("recipes", quietly = TRUE)) install.packages("recipes")
 if (!requireNamespace("Rfast", quietly = TRUE)) install.packages("Rfast")
 
 library(devtools)
-library(manydist)
 # Adjust the path to your package root as needed
 # devtools::load_all("../manydist")
-library(tidyverse)
-library(tidymodels)
-library(palmerpenguins)
 set.seed(123)
 
 
@@ -83,16 +65,13 @@ cl_mod_gow = spectral_from_dist(D_mod_gow, k=myk,affinity_method = "gaussian")
 # cl_dkss = spectral_from_dist(D_dkss, k=myk)
 
 
-truth = c(rep(1,200),rep(2,300),rep(3,400))
-adjustedRandIndex(cl_int, truth)
-adjustedRandIndex(cl_no_int, truth)
+truth=peng |> pull(species)
+
+mclust::adjustedRandIndex(cl_int, truth)
+mclust::adjustedRandIndex(cl_no_int, truth)
 # adjustedRandIndex(cl_gudmm, truth)
-adjustedRandIndex(cl_gow, truth)
-adjustedRandIndex(cl_mod_gow, truth)
-adjustedRandIndex(cl_naive, truth)
+mclust::adjustedRandIndex(cl_gow, truth)
+mclust::adjustedRandIndex(cl_mod_gow, truth)
+mclust::adjustedRandIndex(cl_naive, truth)
 # adjustedRandIndex(cl_dkss, truth)
 
-# describe how data is being generated and under what conditions the interaction term is worth
-dist_list=list(ab_int=D_int,ab_no_int=D_no_int,naive=D_naive,gower=D_gow)
-pairs(dfc[,1:4],col=truth)
-save(dist_list,file="dist_list_toy.RData")
